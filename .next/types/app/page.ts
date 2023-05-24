9fbf1a1ff7c26390abd6ca346ec63c78a9228e2e
Joaquin Ignacio De la Torre <joaquindlt16@gmail.com>
2023-05-24 22:58:10 +0200

    migrated to next and tailwid

--- .next/types/app/page.ts
+++ .next/types/app/page.ts
@@ -1,8 +1,8 @@
-// File: /Users/joaquindelatorre/Desktop/Proyects/Portfolio_Joaquinidlt/app/page.jsx
-import * as entry from '../../../app/page.js'
+// File: /Users/joaquindelatorre/Desktop/Proyects/Portfolio_Joaquinidlt/src/app/page.tsx
+import * as entry from '../../../src/app/page.js'
 import type { ResolvingMetadata } from 'next/dist/lib/metadata/types/metadata-interface.js'
 
-type TEntry = typeof import('../../../app/page.js')
+type TEntry = typeof import('../../../src/app/page.js')
 
 // Check that the entry is a valid entry
 checkFields<Diff<{
